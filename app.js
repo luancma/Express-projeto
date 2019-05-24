f5fe4bdfca9f064d7f9c9acb36849a9f68a0b941
Luan Carlos <luancma18@gmail.com>
2019-05-24 13:57:43 -0300

    teste

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const app = express();
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(adminRoutes);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+
+app.use(shopRoutes);
+app.use('/admin', adminRoutes);
+
+app.use((req,res,next) => {
+    res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+});
+
+app.listen(3333);
